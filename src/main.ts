a631f44bbdfde384b3faac3c5c32ec9215a8030b
jinjoo-jung <wjdwlswn23@naver.com>
2023-09-01 17:38:25 +0900

    Alias

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,28 @@
-interface UserA {
-  name: string;
-  age: number;
-}
-interface UserB extends UserA {
-  isValid: boolean;
-}
-const heropy: UserA = {
-  name: "heropy",
-  age: 20,
-};
-const neo: UserB = {
-  name: "neo",
-  age: 20,
+type TypeA = string;
+type TypeB = string | number | boolean;
+type User =
+  | {
+      name: string;
+      age: number;
+      isValid: boolean;
+    }
+  | [string, number, boolean];
+
+const userA: User = {
+  name: "John",
+  age: 30,
   isValid: true,
 };
+
+const userB: User = ["John", 30, true];
+
+function someFunc(param: TypeB): TypeA {
+  switch (typeof param) {
+    case "string":
+      return param.toUpperCase();
+    case "number":
+      return param.toFixed(2);
+    default:
+      return true;
+  }
+}
