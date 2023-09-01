6fd55822d7b08b8fc534eda40c33a5ab17f6337f
jinjoo-jung <wjdwlswn23@naver.com>
2023-09-01 18:05:13 +0900

    타입 선언

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-interface Cat {
-  name: string;
-  age: number;
+// 1)
+function add(a: string, b: string): string; // 타입 선언
+function add(a: number, b: number): number; // 타입 선언
+function add(a: any, b: any) {
+  // 함수 구현
+  return a + b;
 }
 
-const cat: Cat = {
-  name: "Lucy",
-  age: 3,
-};
-
-function hello(this: Cat, message: string) {
-  // 화살표 함수가 아닌 일반 함수에서 this는 호출되는 위치에서 정의가 된다.
-  console.log(`Hello ${this.name}, ${message}`);
-}
-hello.call(cat, "you are preetty cool!");
-// 어떤 대상에서 실행이 되고 그 대상을 첫번째 인수로 넣고 두번째 인수로 문자데이터
+add("hello", "world"); // hello world
+add(1, 2); // 3
+add("hello", 2); //에러
+add("hello", 2); //에러
