54565de76a07c6be9c87325acdc7b05a8c086bff
jinjoo-jung <wjdwlswn23@naver.com>
2023-09-01 17:56:36 +0900

    명시적 this

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-type TypeUser = {
+interface Cat {
   name: string;
   age: number;
-  isValid: boolean;
-};
-
-interface InterfaceUser {
-  name: string;
-  age: number;
-  isValid: boolean;
 }
 
-const heropy: InterfaceUser = {
-  name: "Heropy",
-  age: 20,
-  isValid: true,
+const cat: Cat = {
+  name: "Lucy",
+  age: 3,
 };
+
+function hello(this: Cat, message: string) {
+  // 화살표 함수가 아닌 일반 함수에서 this는 호출되는 위치에서 정의가 된다.
+  console.log(`Hello ${this.name}, ${message}`);
+}
+hello.call(cat, "you are preetty cool!");
+// 어떤 대상에서 실행이 되고 그 대상을 첫번째 인수로 넣고 두번째 인수로 문자데이터
