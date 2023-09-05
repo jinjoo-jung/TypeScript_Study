a502c2a5b675c195eed9687c4aeb53fa1573258a
jinjoo-jung <wjdwlswn23@naver.com>
2023-09-05 23:40:36 +0900

    server 주석

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,1 @@
-import { getFullName, User } from "./user";
 
-const heropy: User = {
-  firstName: "Heropy",
-  lastName: "Park",
-  age: 36,
-  isValid: true,
-};
-
-const fullName = getFullName(heropy);
-
-console.log(fullName);
-console.log(heropy.isValid);
